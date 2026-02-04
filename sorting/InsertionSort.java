34dd5484389efb03c5fd0808402bcd60249176ec
RuhanMitra <ruhanmitra101@gmail.com>
2026-02-04 05:57:21 +0000

    finished the sorting code

--- sorting/InsertionSort.java
+++ sorting/InsertionSort.java
@@ -1,8 +1,21 @@
 package sorting;
 
 public class InsertionSort implements Sorter {
-
-    public void sort(int[] input) {
-        System.out.println("Insertion Sort!!!");
-    }
+   public void algorithm(int[] input) {
+       System.out.println("Insertion Sort!!!");
+   }
+  
+   public void sort(int[] input) {
+       for (int i = 1; i < input.length; i++) {
+           int temp = input[i];
+          
+           int r = i - 1;
+           while (r >= 0 && input[r] > temp) {
+               input[r+1] = input[r];
+               r--;
+           }
+           input[r+1] = temp;
+       }
+   }
 }
+
--- sorting/SelectionSort.java
+++ sorting/SelectionSort.java
@@ -1,8 +1,22 @@
 package sorting;
 
 public class SelectionSort implements Sorter {
+   public void algorithm(int[] input) {
+       System.out.println("Selection Sort!!!");
+   }
 
-    public void sort(int[] input) {
-        System.out.println("Selection Sort!!!");
-    }
+
+   public void sort(int[] input) {
+       for (int i = 0; i < input.length - 1; i++) {
+           int minIndex = i;
+           for (int r = i + 1; r < input.length; r++) {
+               if (input[r] < input[minIndex]) {
+                   minIndex = r;
+               }
+           }
+           int temp = input[minIndex];
+           input[minIndex] = input[i];
+           input[i] = temp;
+       }
+   }
 }
